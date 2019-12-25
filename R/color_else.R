3c73d73fbca8bcb9cb0aafe7edfb3e0c34d39a3f
nyzhoulang <nyzhoulang@163.com>
2019-12-25 17:06:21 +0800

    simplified function

--- R/color_else.R
+++ R/color_else.R
@@ -1,18 +1,11 @@
 ## @param color A data frame created manually for color assignment.
-color_scheme_ <- function(color){
-  if(grepl("AA", color)){
-    col<- scheme_AA[[color]]
-    names(col) <- scheme_AA[,1]  ##  The name of aminio acid.
-  }
-  else{
-    col <- scheme_NT[[color]]
-    names(col) <- scheme_NT[,1] ## The name of bases.
-  }
-  return(col)
-}
 
 color_scheme <- function(y, color) {
-    scheme <- color_scheme_(color)
-    y$color <- scheme[y$character]
-    return(y)
+    if(grepl("AA", color)){
+        y$color <- scheme_AA[y$character, color]
+    }
+    else{
+        y$color <- scheme_NT[y$character, color]
+    }
+  return(y)
 }
