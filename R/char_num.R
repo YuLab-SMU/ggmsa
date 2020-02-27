3792268f0c837e9babd477b1585fa0478693a757
nyzhoulang <46915370+nyzhoulang@users.noreply.github.com>
2020-02-27 19:13:59 +0800

    update

--- R/char_num.R
+++ R/char_num.R
@@ -9,20 +9,18 @@
 nt_num <- function(data){
     tidy <- data
     col_num <- levels(factor(tidy$position)) # the column number
-    lchar_num <- sapply(1:length(col_num),simplify = F,function(j){
+    lchar_num <- lapply(1:length(col_num),function(j){
         clo <- tidy[tidy$position == j, ] 
-        num <- table(clo$character)
-        if  (!"A" %in% names(num)){num["A"] <- 0}
-        if  (!"C" %in% names(num)){num["C"] <- 0}
-        if  (!"G" %in% names(num)){num["G"] <- 0}
-        if  (!"T" %in% names(num)){num["T"] <- 0}
+        y <- prop.table(table(clo$character))
+        num <-setNames(rep(0,4), c("A", "T", "G", "C"))
+        num[names(y)] <- y
         return(num)
     })
     
-    char_num <- do.call(dplyr::bind_rows,lchar_num)
+    char_num <- do.call(rbind,lchar_num)
     char_num <- as.data.frame(char_num)
     char_num["position"] =  as.numeric(col_num) 
-    char_num2 <- gather(char_num, character, count, "A", "T", "C","G")
+    char_num2 <- gather(char_num, character, fre, "A", "T", "C","G")
     return(char_num2)
 }
 
