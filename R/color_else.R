55a8c9b228f3cd48ff806088a45cbb4dbd964b82
nyzhoulang <nyzhoulang@163.com>
2020-03-30 21:24:51 +0800

    added a function--'available_msa()'

--- R/color_else.R
+++ R/color_else.R
@@ -39,3 +39,27 @@ available_colors <- function(){
     message("color schemes for AA sequences currently available:")
     cat("Clustal", names(scheme_AA))
 }
+
+##' This function lists MSA objects currently available that can be used by 'ggmsa'
+##' 
+##' 
+##' @title List MSA objects currently available
+##' @return A character vector of available objects
+##' @examples available_msa()
+##' @export
+##' @author Lang Zhou
+available_msa <- function(){
+    message("files currently available:")
+    cat(".fasta",'\n')
+  
+    message("XStringSet objects from 'Biostrings' package:")
+    cat(supported_msa_class[!grepl("bin", supported_msa_class)],'\n')
+  
+    message("bin objects from 'seqmagick' package:")
+    cat(supported_msa_class[grepl("bin", supported_msa_class)])
+  
+}
+
+
+
+
