39335a2bd6fbabbeb6917aae667fa7acc770ced0
nyzhoulang <nyzhoulang@163.com>
2020-04-07 21:31:54 +0800

    renamed GC&seqlogo

--- R/geom_GC.R
+++ R/geom_GC.R
@@ -1,29 +1,4 @@
 
-##' get nucleotide number
-
-##' @title nucleotide number
-##' @param data  Multiple aligned sequence file or object for representing nucleotide sequences
-##' @return A data frame
-##' @noRd
-##' @author Lang Zhou
-nt_num <- function(data){
-    tidy <- data
-    col_num <- levels(factor(tidy$position)) # the column number
-    lchar_num <- lapply(1:length(col_num),function(j){
-        clo <- tidy[tidy$position == j, ] 
-        y <- prop.table(table(clo$character))
-        y["GC"] <- y["G"] + y["C"]
-        num <-setNames(rep(0,5), c("A", "T", "G", "C", "GC"))
-        num[names(y)] <- y
-        return(num)
-    })
-    
-    char_num <- do.call(rbind,lchar_num)
-    char_num <- as.data.frame(char_num)
-    char_num["position"] =  as.numeric(col_num) 
-    char_num2 <- gather(char_num, character, fre, "A", "T", "C","G","GC")
-    return(char_num2)
-}
 
 ##' get GC content
 
