9bfb74995d6a860ee6fcf437e8e81a793a90de4f
nyzhoulang <nyzhoulang@163.com>
2020-04-23 19:05:13 +0800

    add param 'seq_name'

--- R/ggmsa.R
+++ R/ggmsa.R
@@ -6,11 +6,14 @@
 ##' representing either nucleotide sequences or peptide sequences.
 ##' @param start a numeric.Start position to plot.
 ##' @param end a numeric. End position to plot.
-##' @param font font families, possible values are 'helvetical', 'mono', and 'DroidSansMono', 'TimesNewRoman'.  Defaults is 'helvetical'. If you specify font = NULL, only the background box will be printed.
+##' @param font font families, possible values are 'helvetical', 'mono', and 'DroidSansMono', 'TimesNewRoman'.  Defaults is 'helvetical'. If font = NULL, only plot the background tile 
 ##' @param color A Color scheme. One of 'Clustal', 'Chemistry_AA', 'Shapely_AA', 'Zappo_AA', 'Taylor_AA', 'Chemistry_NT', 'Shapely_NT', 'Zappo_NT', 'Taylor_NT'. Defaults is 'Clustal'.
 ##' @param char_width characters width. Defaults is 0.9.
 ##' @param none_bg a logical value indicating whether backgroud should be produced. Defaults is FALSE.
 ##' @param posHighligthed A numeric vector of the position that need to be highlighted.
+##' @param seq_name a logical value indicating whether seqence names should be displayed.
+##'  Defaults is 'NULL' which indicates that the sequence name is displayed when 'font = null', but 'font = char' will not be displayed.
+##'  If 'seq_name = TRUE' the sequence name will be displayed in any case. If 'seq_name = FALSE' the sequence name will not be displayed under any circumstances.
 ##' @return ggplot object
 ##' @importFrom tidyr gather
 ##' @importFrom ggplot2 ggplot
@@ -26,6 +29,7 @@
 ##' @importFrom ggplot2 element_blank
 ##' @importFrom magrittr %>%
 ##' @importFrom stats setNames
+##' @importFrom grid unit
 ##' @examples 
 ##' #plot multiple sequences by loading fasta format 
 ##' fasta <- system.file("extdata", "sample.fasta", package = "ggmsa")
@@ -44,9 +48,33 @@
 ##' ggmsa(AAbin, 164, 213, color="Chemistry_AA")
 ##' @export
 ##' @author Guangchuang Yu
-ggmsa <- function(msa, start=NULL, end=NULL, font = "helvetical", color = "Clustal", char_width = 0.9, none_bg = FALSE, posHighligthed = NULL) {
+ggmsa <- function(msa, start=NULL, end=NULL, font = "helvetical", color = "Clustal", char_width = 0.9, none_bg = FALSE, posHighligthed = NULL, seq_name = NULL) {
     data <- tidy_msa(msa, start = start, end = end)
-
-    ggplot() + geom_msa(data, font = font, color = color, char_width = char_width, none_bg = none_bg, posHighligthed = posHighligthed) + 
-        theme_minimal() + xlab(NULL) + ylab(NULL) + theme(legend.position='none') + coord_fixed()
+    
+    ggplot() + geom_msa(data, font = font, 
+                        color = color, 
+                        char_width = char_width, 
+                        none_bg = none_bg, 
+                        posHighligthed = posHighligthed, 
+                        seq_name = seq_name) + 
+               theme_msa()
+               
+ 
 }
+
+##' @importFrom ggplot2 theme_minimal
+theme_msa <- function() {
+    list(xlab(NULL), 
+         ylab(NULL),
+         coord_fixed(),
+         theme_minimal() + 
+         theme(legend.position='none', 
+               strip.text = element_blank(), 
+               panel.spacing.y = unit(.4, "in"))
+         )
+}
+
+
+
+
+  
