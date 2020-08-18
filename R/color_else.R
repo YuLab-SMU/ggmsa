c736dbbf3ecd64a8ce921b9474a699fcd44ec7b3
ShixiangWang <wangshx@shanghaitech.edu.cn>
2020-08-18 14:25:12 +0800

    Add color map LETTER and CN6 for ggmsa

--- R/color_else.R
+++ R/color_else.R
@@ -1,12 +1,31 @@
 ## @param color A data frame created manually for color assignment.
 
 color_scheme <- function(y, color) {
-    if(grepl("AA", color)){
-        y$color <- scheme_AA[y$character, color]
+  if(grepl("AA", color)){
+    y$color <- scheme_AA[y$character, color]
+  }
+  else{
+    if (color == "LETTER") {
+      # https://en.wikipedia.org/wiki/Help:Distinguishable_colors
+      colmap <- c("#FF5005", "#FFFF80", "#990000", "#740AFF", "#E0FF66",
+                  "#00998F", "#5EF1F2", "#FF0010", "#426600", "#FFA8BB",
+                  "#FFA405", "#003380", "#C20088", "#9DCC00", "#8F7C00",
+                  "#94FFB5", "#94FFB5", "#FFCC99", "#2BCE48", "#005C31",
+                  "#191919", "#4C005C", "#993F00", "#0075DC", "#F0A3FF",
+                  "#000000", "#FFFFFF")
+      names(colmap) <- c(LETTERS, "-")
+      y$color <- colmap[y$character]
+    } else if (color == "CN6") {
+      colmap <- c("#0075DC", "#4A98C9", "#000000",
+                  "#FC8A6A", "#F14432", "#BC191A",
+                  "#FFFFFF")
+      names(colmap) <- c(LETTERS[1:6], "-")
+      y$color <- colmap[y$character]
     }
-    else{
-        y$color <- scheme_NT[y$character, color]
+    else {
+      y$color <- scheme_NT[y$character, color]
     }
+  }
   return(y)
 }
 
--- R/msa_data.R
+++ R/msa_data.R
@@ -1,10 +1,11 @@
-##' This function parses FASTA file and convert it to a tidy data frame. 
+##' This function parses FASTA file and convert it to a tidy data frame.
 ##' The function will also assign color to each molecule (amino acid or nucleotide) according to the selected color scheme. Sequence logo data for drawing alignment label will also be added if font != NULL. The output of msa_data() is the input of geom_msa().
 ##'
 ##' @title msa_data
 ##' @param fasta Aligned fasta file.
 ##' @param font font families, possible values are 'helvetical', 'mono', and 'DroidSansMono', 'TimesNewRoman'. . Defaults is 'helvetical'. If you specify font = NULL, only the background box will be printed.
-##' @param color A Color scheme. One of 'Clustal', 'Chemistry_AA', 'Shapely_AA', 'Zappo_AA', 'Taylor_AA', 'Chemistry_NT', 'Shapely_NT', 'Zappo_NT', 'Taylor_NT'.Defaults is 'Clustal'.
+##' @param color A Color scheme. One of 'Clustal', 'Chemistry_AA', 'Shapely_AA', 'Zappo_AA', 'Taylor_AA', 'Chemistry_NT', 'Shapely_NT', 'Zappo_NT', 'Taylor_NT',
+##' "LETTER", "CN6". Defaults is 'Clustal'.
 ##' @param char_width characters width. Defaults is 0.9.
 ##' @return A data frame
 ##' @examples
@@ -15,7 +16,7 @@
 ##' @author Guangchuang Yu
 msa_data <- function(tidymsa, font = "helvetical", color = "Clustal", char_width = 0.9) {
     color <- match.arg(color, c("Clustal","Chemistry_AA","Shapely_AA","Zappo_AA","Taylor_AA",
-                                "Chemistry_NT","Shapely_NT","Zappo_NT","Taylor_NT" ))
+                                "Chemistry_NT","Shapely_NT","Zappo_NT","Taylor_NT", "LETTER", "CN6"))
 
     y <- tidymsa
 
@@ -28,9 +29,9 @@ msa_data <- function(tidymsa, font = "helvetical", color = "Clustal", char_width
     if (is.null(font)) {
         return(y)
     }
-    
+
     font_f <- font_fam[[font]]
-    data_sp <- font_f[unique(y$character)] ## calling internal outline polygons 
+    data_sp <- font_f[unique(y$character)] ## calling internal outline polygons
 
     if (!'name' %in% names(y)) {
         if ('label' %in% names(y)) {
@@ -48,21 +49,21 @@ msa_data <- function(tidymsa, font = "helvetical", color = "Clustal", char_width
         d <- y[i, ]
         dd <- data_sp[[d$character]]
         char_scale <- diff(range(dd$x))/diff(range(dd$y))#equal proportion
-        
-        if ( diff(range(dd$x)) <= diff(range(dd$y)) ){#y-width = char_width, x-width scaled proportionally 
+
+        if ( diff(range(dd$x)) <= diff(range(dd$y)) ){#y-width = char_width, x-width scaled proportionally
             dd$x <- dd$x * (char_width * char_scale)/diff(range(dd$x))
             dd$y <- dd$y * char_width/diff(range(dd$y))
 
             dd$x <- dd$x - min(dd$x) + d$position - (char_width * char_scale)/2
             dd$y <- dd$y - min(dd$y) + d$ypos - char_width/2
-        }else{                                        #x-width = char_width, y-width scaled proportionally 
+        }else{                                        #x-width = char_width, y-width scaled proportionally
             dd$x <- dd$x * char_width/diff(range(dd$x))
             dd$y <- dd$y * char_width/(diff(range(dd$y)) * char_scale)
-          
+
             dd$x <- dd$x - min(dd$x) + d$position - char_width/2
             dd$y <- dd$y - min(dd$y) + d$ypos - (char_width/char_scale)/2
          }
-        
+
         cn <- colnames(d)
         cn <- cn[!cn %in% c('x','y', 'ypos')]
         for (nn in cn) {
@@ -106,23 +107,23 @@ tidy_msa <- function(msa, start = NULL, end = NULL) {
     ## for DNAbin and AAbin
     ## alnmat <- lapply(seq_along(aln), function(i) as.character(aln[[i]])) %>% do.call('rbind',. )
     alndf <- as.data.frame(alnmat, stringsAsFactors = F)
-    
+
     alndf$name = names(aln)
-    
+
     cn = colnames(alndf)
     cn <- cn[!cn %in% "name"]
     df <- gather(alndf, "position", "character", cn)
-    
+
     y <- df
     y$position = as.numeric(sub("V", "", y$position))
     y$character = toupper(y$character)
-     
+
     y$name = factor(y$name, levels=rev(names(aln)))
 
-    
+
     if (is.null(start)) start <- min(y$position)
     if (is.null(end)) end <- max(y$position)
-    
+
     y <- y[y$position >=start & y$position <= end, ]
 
     return(y)
@@ -137,8 +138,8 @@ msa2tidy <- function(msaData) {
   if ("order" %in% names(msaData)) {
     msaData <- msaData[msaData$order == 1,]
   }
-  df_tidy <- data.frame(name = msaData$name, 
-                        position = msaData$position, 
+  df_tidy <- data.frame(name = msaData$name,
+                        position = msaData$position,
                         character = msaData$character)
   return(df_tidy)
 }
